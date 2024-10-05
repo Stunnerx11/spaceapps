1738c57864dc2d8b2c160a3bad6c8230257ae071
Stunnerx11 <168223868+Stunnerx11@users.noreply.github.com>
2024-10-05 18:12:08 +0530

    Update mainloop.js

--- js/orrery/src/mainloop.js
+++ js/orrery/src/mainloop.js
@@ -1,6 +1,6 @@
 import * as ORR from "./init.js";
-import * as THREE from "../../../node_modules/three/build/three.module.js";
-import * as TWEEN from "../../tween/tween.esm.js";
+import * as THREE from "./node_modules/three/build/three.module.js";
+import * as TWEEN from "./tween/tween.esm.js";
 
 export let liveData = false;
 const tempLabels = [];
